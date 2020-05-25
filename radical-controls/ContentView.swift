4c84e1410fbe4fd9a3263d8343245f02d624c0ff
Kevin Logue <kevin.logue@gmail.com>
2020-05-25 15:38:55 +0100

    Initial Commit

---
+++ radical-controls/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  radical-controls
+//
+//  Created by Kevin Logue on 25/05/2020.
+//  Copyright © 2020 Kevin Logue. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ radical-controlsTests/radical_controlsTests.swift
@@ -0,0 +1,34 @@
+//
+//  radical_controlsTests.swift
+//  radical-controlsTests
+//
+//  Created by Kevin Logue on 25/05/2020.
+//  Copyright © 2020 Kevin Logue. All rights reserved.
+//
+
+import XCTest
+@testable import radical_controls
+
+class radical_controlsTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
